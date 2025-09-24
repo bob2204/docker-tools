96a43a8b4559687f5d9d72cbedcc2c19b9b86548
Bruno Guérin <bguerin@dawan.fr>
2025-09-24 08:05:59 +0200

    [UPDATE] Yamllint et plugin ALE

--- vim-config.sh
+++ vim-config.sh
@@ -24,6 +24,22 @@ cd ~/.vim/pack/tpope/start
 git clone https://tpope.io/vim/surround.git
 vim -u NONE -c "helptags surround/doc" -c q
 
+# Plugin ALE
+# https://github.com/dense-analysis/ale
+# https://www.arthurkoziel.com/setting-up-vim-for-yaml/
+
+sudo apt install yamllint
+mkdir -p ~/.config/yamllint
+cat > ~/.config/yamllint/config <<EOF
+extends: relaxed
+
+rules:
+  line-length: disable
+EOF
+
+mkdir -p ~/.vim/pack/git-plugins/start
+git clone --depth 1 https://github.com/dense-analysis/ale.git ~/.vim/pack/git-plugins/start/ale
+
 # Création du fichier
 
 cat > ~/.vimrc <<EOF
@@ -31,4 +47,8 @@ runtime defaults.vim
 let g:indentLine_char = "▏"
 set nu
 colors desert
+let g:ale_echo_msg_format = '[%linter%] %s [%severity%]'
+let g:ale_sign_error = '✘'
+let g:ale_sign_warning = '⚠'
+let g:ale_lint_on_text_changed = 'never'
 EOF
