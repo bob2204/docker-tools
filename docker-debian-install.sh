dd561298faa3b19c551c61c19281b86b68b8c403
Bruno Guérin <br.guerin@free.fr>
2026-02-16 09:28:39 +0100

    [UPDATE] Debian sources.list: New format

--- docker-debian-install.sh
+++ docker-debian-install.sh
@@ -9,6 +9,13 @@ apt update
 apt install -y ca-certificates gpg curl
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
 apt update
 apt install -y docker-ce
