47902b5f6b875eff165230d330459c8992c8251b
bob2204 <78809253+bob2204@users.noreply.github.com>
2026-05-18 13:43:09 +0200

    Update docker-debian-install.sh

--- docker-debian-install.sh
+++ docker-debian-install.sh
@@ -5,8 +5,8 @@
 # 2022-09-22
 #
 
-apt update
-apt install -y ca-certificates gpg curl
+sudo apt update
+sudo apt install -y ca-certificates gpg curl
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -17,5 +17,5 @@ Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
 Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
-apt update
-apt install -y docker-ce
+sudo apt update
+sudo apt install -y docker-ce
